9667077da0dda54282362b3429c22cd7efbd6f16
erinsb <esimonsbrown@gmail.com>
2016-04-03 12:05:45 -0600

    Added more functions and defines to serial_internal.h

--- firmware/serial_internal.h
+++ firmware/serial_internal.h
@@ -42,6 +42,35 @@
 #define NRF_ERROR_SOC_BASE_NUM  (0x2000)    ///< SoC error base
 #define NRF_ERROR_STK_BASE_NUM  (0x3000)    ///< STK error base
 
+#define MESH_UUID                           (0xFEE4)
+#define MESH_ADV_DATA_TYPE                  (0x16)
+#define BLE_ADV_PACKET_PAYLOAD_MAX_LENGTH   (31)
+
+#define CACHE_TASK_FIFO_SIZE            (8)
+
+
+#if defined(__CC_ARM)
+
+/* ARMCC and GCC have different ordering for packed typedefs, must separate macros */
+    #define __packed_gcc
+    #define __packed_armcc __packed
+
+    #define _DISABLE_IRQS(_was_masked) _was_masked = __disable_irq()
+    #define _ENABLE_IRQS(_was_masked) if (!_was_masked) { __enable_irq(); }
+
+#elif defined(__GNUC__)
+
+    #define __packed_armcc
+    #define __packed_gcc __attribute__((packed))
+
+    #define _DISABLE_IRQS(_was_masked) do{ \
+        __ASM volatile ("MRS %0, primask" : "=r" (_was_masked) );\
+        __ASM volatile ("cpsid i" : : : "memory");\
+    } while(0)
+
+    #define _ENABLE_IRQS(_was_masked) if (!_was_masked) { __enable_irq(); }
+
+
 
 /** @brief Default value for the number of handle cache entries */
 #ifndef RBC_MESH_HANDLE_CACHE_ENTRIES
@@ -72,6 +101,7 @@ static uint32_t         m_handle_cache_head;
 static uint32_t         m_handle_cache_tail;
 static bool             m_is_initialized = false;
 static bool             m_handle_task_scheduled;
+static uint32_t 		g_critical = 0;
 
 #define HANDLE_CACHE_ITERATE(index)     do { index = m_handle_cache[index].index_next; } while (0)
 #define HANDLE_CACHE_ITERATE_BACK(index)     do { index = m_handle_cache[index].index_prev; } while (0)
@@ -85,6 +115,53 @@ static uint16_t handle_entry_get(rbc_mesh_value_handle_t handle);
 
 /* specialized function pointer for copying memory between two instances */
 typedef void (*fifo_memcpy)(void* dest, const void* src);
+
+typedef enum {
+/* -------------------  Cortex-M0 Processor Exceptions Numbers  ------------------- */
+  Reset_IRQn                    = -15,              /*!<   1  Reset Vector, invoked on Power up and warm reset                 */
+  NonMaskableInt_IRQn           = -14,              /*!<   2  Non maskable Interrupt, cannot be stopped or preempted           */
+  HardFault_IRQn                = -13,              /*!<   3  Hard Fault, all classes of Fault                                 */
+  SVCall_IRQn                   =  -5,              /*!<  11  System Service Call via SVC instruction                          */
+  DebugMonitor_IRQn             =  -4,              /*!<  12  Debug Monitor                                                    */
+  PendSV_IRQn                   =  -2,              /*!<  14  Pendable request for system service                              */
+  SysTick_IRQn                  =  -1,              /*!<  15  System Tick Timer                                                */
+/* ----------------------  nrf51 Specific Interrupt Numbers  ---------------------- */
+  POWER_CLOCK_IRQn              =   0,              /*!<   0  POWER_CLOCK                                                      */
+  RADIO_IRQn                    =   1,              /*!<   1  RADIO                                                            */
+  UART0_IRQn                    =   2,              /*!<   2  UART0                                                            */
+  SPI0_TWI0_IRQn                =   3,              /*!<   3  SPI0_TWI0                                                        */
+  SPI1_TWI1_IRQn                =   4,              /*!<   4  SPI1_TWI1                                                        */
+  GPIOTE_IRQn                   =   6,              /*!<   6  GPIOTE                                                           */
+  ADC_IRQn                      =   7,              /*!<   7  ADC                                                              */
+  TIMER0_IRQn                   =   8,              /*!<   8  TIMER0                                                           */
+  TIMER1_IRQn                   =   9,              /*!<   9  TIMER1                                                           */
+  TIMER2_IRQn                   =  10,              /*!<  10  TIMER2                                                           */
+  RTC0_IRQn                     =  11,              /*!<  11  RTC0                                                             */
+  TEMP_IRQn                     =  12,              /*!<  12  TEMP                                                             */
+  RNG_IRQn                      =  13,              /*!<  13  RNG                                                              */
+  ECB_IRQn                      =  14,              /*!<  14  ECB                                                              */
+  CCM_AAR_IRQn                  =  15,              /*!<  15  CCM_AAR                                                          */
+  WDT_IRQn                      =  16,              /*!<  16  WDT                                                              */
+  RTC1_IRQn                     =  17,              /*!<  17  RTC1                                                             */
+  QDEC_IRQn                     =  18,              /*!<  18  QDEC                                                             */
+  LPCOMP_IRQn                   =  19,              /*!<  19  LPCOMP                                                           */
+  SWI0_IRQn                     =  20,              /*!<  20  SWI0                                                             */
+  SWI1_IRQn                     =  21,              /*!<  21  SWI1                                                             */
+  SWI2_IRQn                     =  22,              /*!<  22  SWI2                                                             */
+  SWI3_IRQn                     =  23,              /*!<  23  SWI3                                                             */
+  SWI4_IRQn                     =  24,              /*!<  24  SWI4                                                             */
+  SWI5_IRQn                     =  25               /*!<  25  SWI5                                                             */
+} IRQn_Type;
+
+typedef __packed_armcc struct
+{
+    uint8_t type : 4;
+    uint8_t _rfu1 : 2;
+    uint8_t addr_type : 1;
+    uint8_t _rfu2 : 1;
+    uint8_t length;
+    uint8_t _rfu3;
+} __packed_gcc ble_packet_header_t;
 
 typedef __packed_armcc struct
 {
