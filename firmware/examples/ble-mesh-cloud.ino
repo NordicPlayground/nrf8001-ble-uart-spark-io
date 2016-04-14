aa4ad46447a6fb6888509d012f33120e1d774a1e
erinsb <esimonsbrown@gmail.com>
2016-04-14 11:24:27 -0600

    updated main

--- firmware/examples/ble-mesh-cloud.ino
+++ firmware/examples/ble-mesh-cloud.ino
@@ -1,3 +1,147 @@
+// This #include statement was automatically added by the Particle IDE.
+#include "nrf8001-ble-uart-spark-io/nrf8001-ble-uart-spark-io.h"
+
+// This #include statement was automatically added by the Particle IDE.
+#include "nrf8001-ble-uart-spark-io/nrf8001-ble-uart-spark-io.h"
+
+// This #include statement was automatically added by the Particle IDE.
+#include "nrf8001-ble-uart-spark-io/nrf8001-ble-uart-spark-io.h"
+
+// This #include statement was automatically added by the Particle IDE.
+#include "nrf8001-ble-uart-spark-io/nrf8001-ble-uart-spark-io.h"
+
+// This #include statement was automatically added by the Particle IDE.
+#include "nrf8001-ble-uart-spark-io/nrf8001-ble-uart-spark-io.h"
+
+// This #include statement was automatically added by the Particle IDE.
+#include "nrf8001-ble-uart-spark-io/nrf8001-ble-uart-spark-io.h"
+
+// This #include statement was automatically added by the Particle IDE.
+#include "nrf8001-ble-uart-spark-io/nrf8001-ble-uart-spark-io.h"
+
+// This #include statement was automatically added by the Particle IDE.
+#include "nrf8001-ble-uart-spark-io/nrf8001-ble-uart-spark-io.h"
+
+// This #include statement was automatically added by the Particle IDE.
+#include "nrf8001-ble-uart-spark-io/nrf8001-ble-uart-spark-io.h"
+
+// This #include statement was automatically added by the Particle IDE.
+#include "nrf8001-ble-uart-spark-io/nrf8001-ble-uart-spark-io.h"
+
+// This #include statement was automatically added by the Particle IDE.
+#include "nrf8001-ble-uart-spark-io/nrf8001-ble-uart-spark-io.h"
+
+// This #include statement was automatically added by the Particle IDE.
+#include "nrf8001-ble-uart-spark-io/nrf8001-ble-uart-spark-io.h"
+
+// This #include statement was automatically added by the Particle IDE.
+#include "nrf8001-ble-uart-spark-io/nrf8001-ble-uart-spark-io.h"
+
+// This #include statementhttps://build.particle.io/build/56eaea97bd9cf0716b00065e#flash was automatically added by the Particle IDE.
+#include "nrf8001-ble-uart-spark-io/nrf8001-ble-uart-spark-io.h"
+
+// This #include statement was automatically added by the Particle IDE.
+#include "nrf8001-ble-uart-spark-io/nrf8001-ble-uart-spark-io.h"
+
+// This #include statement was automatically added by the Particle IDE.
+#include "nrf8001-ble-uart-spark-io/nrf8001-ble-uart-spark-io.h"
+
+// This #include statement was automatically added by the Particle IDE.
+#include "nrf8001-ble-uart-spark-io/nrf8001-ble-uart-spark-io.h"
+
+// This #include statement was automatically added by the Particle IDE.
+#include "nrf8001-ble-uart-spark-io/nrf8001-ble-uart-spark-io.h"
+
+// This #include statement was automatically added by the Particle IDE.
+#include "nrf8001-ble-uart-spark-io/nrf8001-ble-uart-spark-io.h"
+
+// This #include statement was automatically added by the Particle IDE.
+#include "nrf8001-ble-uart-spark-io/nrf8001-ble-uart-spark-io.h"
+
+// This #include statement was automatically added by the Particle IDE.
+#include "nrf8001-ble-uart-spark-io/nrf8001-ble-uart-spark-io.h"
+
+// This #include statement was automatically added by the Particle IDE.
+#include "nrf8001-ble-uart-spark-io/nrf8001-ble-uart-spark-io.h"
+
+// This #include statement was automatically added by the Particle IDE.
+#include "nrf8001-ble-uart-spark-io/nrf8001-ble-uart-spark-io.h"
+
+// This #include statement was automatically added by the Particle IDE.
+#include "nrf8001-ble-uart-spark-io/nrf8001-ble-uart-spark-io.h"
+
+// This #include statement was automatically added by the Particle IDE.
+#include "nrf8001-ble-uart-spark-io/nrf8001-ble-uart-spark-io.h"
+
+// This #include statement was automatically added by the Particle IDE.
+#include "nrf8001-ble-uart-spark-io/nrf8001-ble-uart-spark-io.h"
+
+// This #include statement was automatically added by the Particle IDE.
+#include "nrf8001-ble-uart-spark-io/nrf8001-ble-uart-spark-io.h"
+
+// This #include statement was automatically added by the Particle IDE.
+#include "nrf8001-ble-uart-spark-io/nrf8001-ble-uart-spark-io.h"
+
+// This #include statement was automatically added by the Particle IDE.
+#include "nrf8001-ble-uart-spark-io/nrf8001-ble-uart-spark-io.h"
+
+// This #include statement was automatically added by the Particle IDE.
+#include "nrf8001-ble-uart-spark-io/nrf8001-ble-uart-spark-io.h"
+
+// This #include statement was automatically added by the Particle IDE.
+#include "nrf8001-ble-uart-spark-io/nrf8001-ble-uart-spark-io.h"
+
+// This #include statement was automatically added by the Particle IDE.
+#include "nrf8001-ble-uart-spark-io/nrf8001-ble-uart-spark-io.h"
+
+// This #include statement was automatically added by the Particle IDE.
+#include "nrf8001-ble-uart-spark-io/nrf8001-ble-uart-spark-io.h"
+
+// This #include statement was automatically added by the Particle IDE.
+#include "nrf8001-ble-uart-spark-io/nrf8001-ble-uart-spark-io.h"
+
+// This #include statement was automatically added by the Particle IDE.
+#include "nrf8001-ble-uart-spark-io/nrf8001-ble-uart-spark-io.h"
+
+// This #include statement was automatically added by the Particle IDE.
+#include "nrf8001-ble-uart-spark-io/nrf8001-ble-uart-spark-io.h"
+
+// This #include statement was automatically added by the Particle IDE.
+#include "nrf8001-ble-uart-spark-io/nrf8001-ble-uart-spark-io.h"
+
+// This #include statement was automatically added by the Particle IDE.
+#include "nrf8001-ble-uart-spark-io/nrf8001-ble-uart-spark-io.h"
+
+// This #include statement was automatically added by the Particle IDE.
+#include "nrf8001-ble-uart-spark-io/nrf8001-ble-uart-spark-io.h"
+
+// This #include statement was automatically added by the Particle IDE.
+#include "nrf8001-ble-uart-spark-io/nrf8001-ble-uart-spark-io.h"
+
+// This #include statement was automatically added by the Particle IDE.
+#include "nrf8001-ble-uart-spark-io/nrf8001-ble-uart-spark-io.h"
+
+// This #include statement was automatically added by the Particle IDE.
+#include "nrf8001-ble-uart-spark-io/nrf8001-ble-uart-spark-io.h"
+
+// This #include statement was automatically added by the Particle IDE.
+#include "nrf8001-ble-uart-spark-io/nrf8001-ble-uart-spark-io.h"
+
+// This #include statement was automatically added by the Particle IDE.
+#include "nrf8001-ble-uart-spark-io/nrf8001-ble-uart-spark-io.h"
+
+// This #include statement was automatically added by the Particle IDE.
+#include "nrf8001-ble-uart-spark-io/nrf8001-ble-uart-spark-io.h"
+
+// This #include statement was automatically added by the Particle IDE.
+#include "nrf8001-ble-uart-spark-io/nrf8001-ble-uart-spark-io.h"
+
+// This #include statement was automatically added by the Particle IDE.
+#include "nrf8001-ble-uart-spark-io/nrf8001-ble-uart-spark-io.h"
+
+// This #include statement was automatically added by the Particle IDE.
+#include "nrf8001-ble-uart-spark-io/nrf8001-ble-uart-spark-io.h"
+
 /* Copyright (c) 2014, Nordic Semiconductor ASA
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -29,6 +173,7 @@
 #include "nrf8001-ble-uart-spark-io/serial_evt.h"
 
 #define ACCESS_ADDR     (0xA541A68F)
+//#define ACCESS_ADDR     (e4cf98b0bd299b9ac1e84091626157a9fd6f0002)
 
 // defining the state of the system for communication
 enum state_t{
@@ -39,6 +184,8 @@ enum state_t{
 
 int state = init;
 
+int state_test = 100;
+
 // buffer for the last value send by the SPI slave
 int lastResponse;
 
@@ -48,12 +195,22 @@ int initState = 0;
 int set_val(String args){
 
     if(state != ready){
-        return -1;
+        Serial.println("STATE not ready");
+        Serial.println(state);
+        Serial.println(digitalRead(A2));
+        Serial.println(digitalRead(D5));
+        Serial.println(digitalRead(D2));
+        return -5;
     }
     state = waitingForEvent;
 
-    uint8_t handle = args[0] - '0';
+    //uint8_t handle = args[0] - '0';
+    uint16_t handle = args[0] - '0';
+    //uint8_t value = 0;
     uint8_t value  = args[1] - '0';
+    //uint8_t value = args.toInt();
+    
+   // Serial.printlnf("%p", &value);
     
     return rbc_mesh_value_set(handle, &value, (uint8_t) 1);
 }
@@ -77,6 +234,9 @@ aci_pins_t pins;
 void setup(void)
 {
   Serial.begin(9600);
+  
+  //Serial.println("Setup");
+  //Particle.publish("SETUP");
   
   pins.board_name = BOARD_DEFAULT; //See board.h for details REDBEARLAB_SHIELD_V1_1 or BOARD_DEFAULT
   pins.reqn_pin   = A2;
@@ -102,6 +262,8 @@ void setup(void)
   Spark.function("get_val_req", get_val_req);
   Spark.variable("state", &state, INT);
   Spark.variable("lastResponse", &lastResponse, INT);
+  
+  Serial.println("SETUP DONE");
 
   return;
 }
@@ -111,14 +273,16 @@ void setup(void)
 void initConnectionSlowly(){
     switch(initState) {
         case 0:
-            rbc_mesh_init(ACCESS_ADDR, (uint8_t) 38, (uint8_t) 2, (uint32_t) 100);
+            state_test = rbc_mesh_init(ACCESS_ADDR, (uint8_t) 38, (uint32_t) 100);
             initState++;
             Serial.println("Sent init command");
+            Spark.publish("Sent init command");
             break;
         case 1:
             rbc_mesh_value_enable((uint8_t) 1);
             initState++;
             Serial.println("Enabled value 1");
+            Spark.publish("Enabled value 1");
             break;
         case 2:
             rbc_mesh_value_enable((uint8_t) 2);
@@ -133,27 +297,45 @@ void initConnectionSlowly(){
 
 // arduino conform main loop
 void loop() {
+    //Particle.publish("HERE");
     static bool newMessage = false;
+    
     // send next initialization command to SPI slave until we leave init state
     if (state == init && newMessage) {
+        //Serial.println("here");
+        Particle.publish("New Message");
         initConnectionSlowly();
     }
     
     //Process any ACI commands or events
     serial_evt_t evnt;
     newMessage = rbc_mesh_evt_get(&evnt);
+    delay(100);
+    
+    if(newMessage) {
+        Serial.println("there is a new message");
+        Serial.print("Event: ");
+        Serial.println(evnt.opcode, HEX);
+    }
 
     if(newMessage && evnt.opcode == SERIAL_EVT_OPCODE_CMD_RSP){
         if (evnt.params.cmd_rsp.status != ACI_STATUS_SUCCESS)
         {
             Serial.print("Error response on cmd ");
+            //Serial.print(evnt.params.cmd_rsp.command_opcode);
             Serial.print(evnt.params.cmd_rsp.command_opcode, HEX);
             Serial.print(": ");
             Serial.print(evnt.params.cmd_rsp.status, HEX);
+            //Serial.print(evnt.params.cmd_rsp.status);
+            Serial.println();
+            Serial.println(digitalRead(A2));
+            Serial.println(digitalRead(D5));
+            Serial.println(digitalRead(D2));
             Serial.println();
         }
 
         if (state == waitingForEvent) {
+            Serial.println("Waiting");
             state = ready;
             // save the response value of SPI slave into buffer
             // can be read via callback function
