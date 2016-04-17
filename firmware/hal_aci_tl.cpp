4b4ab46b8faab21ddb94aaf7a8a1afdd2a44039e
erinsb <esimonsbrown@gmail.com>
2016-04-17 13:21:11 -0600

    added debugging lines

--- firmware/hal_aci_tl.cpp
+++ firmware/hal_aci_tl.cpp
@@ -339,7 +339,7 @@ bool hal_aci_tl_send(hal_aci_data_t *p_aci_cmd)
 static uint8_t spi_readwrite(const uint8_t aci_byte)
 {	
   Serial.print("spi_readwrite: ");
-  Serial.println(aci_byte);
+  Serial.println(aci_byte, HEX);
 	return SPI.transfer(aci_byte);
 }
 
